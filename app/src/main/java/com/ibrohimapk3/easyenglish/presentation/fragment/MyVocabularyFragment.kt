acbf5a4dacaf158cec5714eeb733464090221266
Ibrohim Usmonov <ibrohimusmonov365@gmail.com>
2025-04-27 20:41:12 +0500

    presentation

---
+++ app/src/main/java/com/ibrohimapk3/easyenglish/presentation/fragment/MyVocabularyFragment.kt
@@ -0,0 +1,19 @@
+package com.ibrohimapk3.easyenglish.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ibrohimapk3.easyenglish.R
+
+class MyVocabularyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_vocabulary, container, false)
+    }
+
+}
