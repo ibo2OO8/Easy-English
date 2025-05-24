2816fbc7aef4ae2cdbf806771558fb418c336095
Ibrohim Usmonov <ibrohimusmonov365@gmail.com>
2025-05-24 17:30:29 +0500

    my sound is ready

---
+++ app/src/main/java/com/ibrohimapk3/easyenglish/presentation/view/ListOfAlphabet.kt
@@ -0,0 +1,7 @@
+package com.ibrohimapk3.easyenglish.presentation.view
+
+class ListOfAlphabet {
+    var list = arrayListOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    var listAB = ('A'..'Z').toList()
+
+}
