ccb766a34d9e382083afe551bfaba94033f753bf
Ibrohim Usmonov <ibrohimusmonov365@gmail.com>
2025-06-21 21:46:23 +0500

    i fix mitakes

--- app/src/main/java/com/ibrohimapk3/easyenglish/presentation/view/ListOfAlphabet.kt
+++ app/src/main/java/com/ibrohimapk3/easyenglish/presentation/view/ListOfAlphabet.kt
@@ -1,7 +1,34 @@
 package com.ibrohimapk3.easyenglish.presentation.view
 
 class ListOfAlphabet {
-    var list = arrayListOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    var list = arrayListOf(
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z"
+    )
     var listAB = ('A'..'Z').toList()
 
 }
