a4324bf22fc2da36e70baad4391e6f77ff709b8b
Ibrohim Usmonov <ibrohimusmonov365@gmail.com>
2025-08-12 21:48:44 +0500

    im ganna create all vocabulary

--- app/src/main/java/com/ibrohimapk3/easyenglish/presentation/viewmodel/AddNewWordViewModel.kt
+++ app/src/main/java/com/ibrohimapk3/easyenglish/presentation/viewmodel/AddNewWordViewModel.kt
@@ -1,22 +1,10 @@
 package com.ibrohimapk3.easyenglish.presentation.viewmodel
-
-import android.app.Application
-import android.widget.ImageView
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ibrohimapk3.easyenglish.data.WordRepositoryImpl
-import com.ibrohimapk3.easyenglish.data.local.DaoForVocabulary
-import com.ibrohimapk3.easyenglish.data.local.MainDb
-import com.ibrohimapk3.easyenglish.domain.UseCase.AddWordUseCase
-import com.ibrohimapk3.easyenglish.domain.UseCase.DeleteWordUseCase
-import com.ibrohimapk3.easyenglish.domain.UseCase.GetWordUseCase
 import com.ibrohimapk3.easyenglish.domain.VocabularyRepository
 import com.ibrohimapk3.easyenglish.domain.Word
 import kotlinx.coroutines.launch
-
 class AddNewWordViewModel(private val repository: VocabularyRepository) : ViewModel() {
-
     fun addWord(word: Word) {
         viewModelScope.launch {
             repository.addWordList(word)
